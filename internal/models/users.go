fe79ef8e2dbe5aaee8840958a65cea2ce7f56f60
Peng-Yu Chen <walkccray@gmail.com>
2023-04-29 13:26:14 -0400

    feat: 12.2 Request context for authentication/authorization

--- internal/models/users.go
+++ internal/models/users.go
@@ -92,5 +92,10 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 
 // Exists checks if a user exists with a specific ID.
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
