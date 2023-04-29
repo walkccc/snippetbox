ae7840a76d1368b43eae98b8cdeefbe176be0e69
Peng-Yu Chen <walkccray@gmail.com>
2023-04-29 13:49:23 -0400

    feat: 14.1 Unit testing and sub-tests

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -22,6 +22,9 @@ type templateData struct {
 
 // humanDate returns a nicely formatted string representation of a time.Time object.
 func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
 	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
